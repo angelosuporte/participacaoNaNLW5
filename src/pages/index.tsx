884c1abfda8ad89e8032c262926d07b31935637d
Angelo Carvalho <angelosuporteti@gmail.com>
2021-04-24 12:24:18 -0300

    Using Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
@@ -44,7 +45,10 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps ) {
                 />
 
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}> 
+                  <a>{episode.title}</a>
+                  </Link>
+                  
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -65,12 +69,14 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps ) {
 
         <table cellSpacing={0}>
           <thead>
+            <tr>
             <th></th>
             <th>Podcast</th>
             <th>Integrantes</th>
             <th>Data</th>
             <th>Duração</th>
             <th></th>
+            </tr>
           </thead>
           <body>
             {allEpisodes.map(episode => {
@@ -84,7 +90,11 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps ) {
                       objectFit= "cover" />
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+
+                  <Link href={`/episodes/${episode.id}`}> 
+                  <a>{episode.title}</a>
+                  </Link>
+
                   </td>
                   <td>{episode.members}</td>
                   <td style={{ width: 100}}>{episode.publishedAt}</td>
