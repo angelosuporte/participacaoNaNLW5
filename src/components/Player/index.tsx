18cfc27886036f2fc0bf8b7e7fd118c3d9bf8c59
Angelo Carvalho <angelosuporteti@gmail.com>
2021-04-28 14:28:00 -0300

    Atualization in next and previous

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,5 @@
-import { PlayerContext } from '../../context/PlayerContext';
-import { useContext, useEffect, useRef}  from 'react';
+import { usePlayer } from '../../context/PlayerContext';
+import { useEffect, useRef}  from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
@@ -18,8 +18,10 @@ export function Player(){
         togglePlay, 
         setIsPlayingState,
         playNext,
-        playPrevious
-    } = useContext(PlayerContext)
+        playPrevious,
+        hasNext,
+        hasPrevious
+    } = usePlayer();
 
     useEffect(() => {
         if (!audioRef.current){
@@ -93,7 +95,7 @@ export function Player(){
                     <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar" />
                     </button>
-                    <button type="button" onClick={playPrevious} disabled={!episode}>
+                    <button type="button" onClick={playPrevious} disabled={!episode || !hasPrevious}>
                         <img src="/play-previous.svg" alt="Tocar anterior" />
                     </button>
                     <button type="button" 
@@ -105,7 +107,7 @@ export function Player(){
                             ? <img src="/pause.svg" alt="Tocar" />
                             : <img src="/play.svg" alt="Tocar" />}
                     </button>
-                    <button type="button" onClick={playNext} disabled={!episode}>
+                    <button type="button" onClick={playNext} disabled={!episode || hasNext}>
                         <img src="/play-next.svg" alt="Tocar próxima" />
                     </button>
                     <button type="button" disabled={!episode}>
